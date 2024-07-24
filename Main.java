144e7a2eb66d857268b9ff545006129531a17e1e
SRIJANI181 <66792807+SRIJANI181@users.noreply.github.com>
2024-07-24 10:41:57 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
 
         System.out.println("This is java program");
         System.out.println(new Date());
+        System.out.println("This is java program jenkins made by srijani");
         
     }
 }
